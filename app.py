b77b1a44aee8d4a7abc0906b5f0e44f8dc6573f1
King  Curtis <kingcurtis618@KingdeMacBook-Air.local>
2023-12-16 15:54:49 +0800

    renewfile

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def handle_message(event):
             line_bot_api.reply_message(event.reply_token, button_template_message)
         #Branch 1
         #加密者
-        elif user_state[user_id]["state"] == "Encryter":
+        elif user_state[user_id]["state"] == "Encrypter":
             if user_state[user_id]["workflow"] == 0:
                 line_bot_api.reply_message(event.reply_token, TextSendMessage("小明資訊：\n公鑰（public key）：iLoveYou\n發送訊息內容：我想認識你\n\n請遵循上述進行以下任務\n\n請輸入欲發送訊息！！！"))
                 user_state[user_id]["workflow"] += 1
